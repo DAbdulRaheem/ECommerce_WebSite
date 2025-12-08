bbd00e716b7f115f6133a5c67550c75bb152f7ce
DAbdulRaheem <raheemabduld56@gmail.com>
2025-12-08 21:25:30 +0530

    Refactor: Cleaned up settings and moved secrets to env

--- Django/backend/backend/settings.py
+++ Django/backend/backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+214utn=^dx6(c^or$k#!5-is$kh&m7g-(=d05367u=-r89^f'
+# SECRET_KEY = 'django-insecure-*+214utn=^dx6(c^or$k#!5-is$kh&m7g-(=d05367u=-r89^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
